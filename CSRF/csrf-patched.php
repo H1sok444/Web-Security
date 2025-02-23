f72d6026baa941b6f89cd6d1470c716adb693944
H1sok4 <88634883+H1sok444@users.noreply.github.com>
2025-02-23 22:23:51 +0000

    Create csrf-patched.php

---
+++ CSRF/csrf-patched.php
@@ -0,0 +1,11 @@
+//Patched
+<form method="post" action="">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="current_password" required>
+
+        <label for="confirm_password">ConfirmPassword:</label>
+        <input type="password" id="confirm_password" name="confirm_password" required>
+		<input type="hidden" id="csrf_token" name="csrf_token" value="<?php echo $_COOKIE['csrf-token']; ?>">
+        <button type="submit" name="password_submit" >Update Password</button>
+    </form>submit">
+</form>
