9920adb018dd6da92c1ca0e4088a61ef4b26a583
H1sok4 <88634883+H1sok444@users.noreply.github.com>
2025-04-08 14:27:48 +0100

    Update server.py

--- SSRF/server.py
+++ SSRF/server.py
@@ -1,5 +1,5 @@
 # Can be used in Blind SSRF Testing
-from http.server import SimpleHTTPRequestHandler, TTPServer
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 from urllib.parse import unquote
 class CustomRequestHandler(SimpleHTTPRequestHandler):
 
